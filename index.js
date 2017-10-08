37634e0b4dd3f3cf0a4de0e4aa3d4ad47eabae5d
Eirik S. Morland <eirik@morland.no>
2017-10-08 17:48:22 +0200

    Make it possible to subscribe.

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ client.on('error', function(e) {
   throw e;
 });
 
+let subscriber = redis.createClient();
+let callbacks = [];
+subscriber.psubscribe('eo.*');
+subscriber.on('pmessage', (pattern, channel, message) => {
+  callbacks.forEach((cb) => {
+    cb(channel, message);
+  })
+})
+
 module.exports = {
   get: function(key, callback) {
     client.get(key, callback);
@@ -18,5 +27,12 @@ module.exports = {
   },
   del: function(key, callback) {
     client.del(key, callback);
+  },
+  listen: (callback) => {
+    callbacks.push(callback);
+  },
+  append: (value) => {
+    client.rpush('eostorequeue', value);
+    client.publish('eo.appended', value);
   }
 }
